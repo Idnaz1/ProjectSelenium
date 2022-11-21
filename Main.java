f67566b804fce73bbd3a038b4239e6333a372e4a
Idnaz1 <110234011+Idnaz1@users.noreply.github.com>
2022-11-21 21:56:01 +0200

    Update file 21/11/22
    Update file 21/11/22

--- Main.java
+++ Main.java
@@ -15,20 +15,29 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Idan\\Downloads\\chromedriver.exe");
         WebDriver Driver = new ChromeDriver();
+
     }
     @Test
     void registrationProcessTest() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Idan\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // Call to the Webdriver by "driver" var
         driver.get("http://tutorialsninja.com/demo/index.php?route=account/register"); // Get to this site
-        driver.findElement(By.id("input-firstname")).sendKeys("Idan"); // find the element "First name" & write this text
-        driver.findElement(By.id("input-lastname")).sendKeys("Azuri"); // find the element "last name" & write this text
-        driver.findElement(By.id("input-email")).sendKeys("idn.az110@gmail.com"); // find the element "Email" & write this text
-        driver.findElement(By.id("input-telephone")).sendKeys("0545646076"); // find the element "Telephone" & write this text
-        driver.findElement(By.id("input-password")).sendKeys("123456"); // find the element "First name" & write this text
-        driver.findElement(By.id("input-confirm")).sendKeys("123456"); // find the element "Confirm" & write this text
-        driver.findElement(By.name("agree")).click(); // find the element "Agree" & write this text
-        driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/div/input[2]")).click(); // find the element by xpath & click
+        var firstName = driver.findElement(By.id("input-firstname"));
+        firstName.sendKeys("Idan"); // find the element "First name" & write this text
+        var lastName = driver.findElement(By.id("input-lastname"));
+        lastName.sendKeys("Azuri"); // find the element "last name" & write this text
+        var eMail = driver.findElement(By.id("input-email"));
+        eMail.sendKeys("idn.az01110@gmail.com"); // find the element "Email" & write this text
+        var telephoneNub = driver.findElement(By.id("input-telephone"));
+        telephoneNub.sendKeys("0545646076"); // find the element "Telephone" & write this text
+        var passwordNum = driver.findElement(By.id("input-password"));
+        passwordNum.sendKeys("123456"); // find the element "First name" & write this text
+        var confirmBotm = driver.findElement(By.id("input-confirm"));
+        confirmBotm.sendKeys("123456"); // find the element "Confirm" & write this text
+        var agreeBotm = driver.findElement(By.name("agree"));
+        agreeBotm.click(); // find the element "Agree" & write this text
+        var continueBotm = driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/div/input[2]"));
+        continueBotm.click(); // find the element by xpath & click
         Assert.assertEquals("http://tutorialsninja.com/demo/index.php?route=account/success" , driver.getCurrentUrl()); // Check if this is the correct and exact address of the site by matching.
     }
     @Test
@@ -36,9 +45,12 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Idan\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // Call to the Webdriver by "driver" var
         driver.get("http://tutorialsninja.com/demo/index.php?route=account/login"); // Get to this site
-        driver.findElement(By.id("input-email")).sendKeys("idn.az15@gmail.com"); // find the element "Email" & write this text
-        driver.findElement(By.id("input-password")).sendKeys("123456"); // find the element "Password" & write this text
-        driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input")).click();  // find the element by xpath & click
+        var eMail2 = driver.findElement(By.id("input-email"));
+        eMail2.sendKeys("idn.az010155@gmail.com"); // find the element "Email" & write this text
+        var password2 = driver.findElement(By.id("input-password"));
+        password2.sendKeys("123456"); // find the element "Password" & write this text
+        var loginBotm = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input"));
+        loginBotm.click();  // find the element by xpath & click
         Assert.assertEquals("http://tutorialsninja.com/demo/index.php?route=account/login" , driver.getCurrentUrl()); // Check if this is the correct and exact address of the site by matching.
     }
     @Test
@@ -46,8 +58,10 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Idan\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // Call to the Webdriver by "driver" var
         driver.get("http://tutorialsninja.com/demo/index.php?route=product/product&product_id=43"); // Get to this site
-        driver.findElement(By.id("button-cart")).click(); // find the element by id & click
+        var addToCartBotm = driver.findElement(By.id("button-cart"));
+        addToCartBotm.click(); // find the element by id & click
         driver.get("http://tutorialsninja.com/demo/index.php?route=product/product&product_id=33"); // Get to this site
-        driver.findElement(By.id("button-cart")).click(); // find the element by id & click
+        var addToCartBotm2 = driver.findElement(By.id("button-cart"));
+        addToCartBotm2.click(); // find the element by id & click
     }
 }
